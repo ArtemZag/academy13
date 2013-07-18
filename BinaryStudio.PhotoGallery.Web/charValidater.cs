b0122ffadb2e03da13f38dad4ec6ad34d32ceb62
San4ezSoft <aleksandr.nosov.1991@gmail.com>
2013-07-18 18:31:14 +0300

    Added charValidater for ViewModel

---
+++ BinaryStudio.PhotoGallery.Web/charValidater.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BinaryStudio.PhotoGallery.Web
+{
+    internal delegate bool isValid(char ch);
+}
