326b45d521b79e3cb7e3b99c36d2c3763e04163a
San4ezSoft <aleksandr.nosov.1991@gmail.com>
2013-07-18 18:10:32 +0300

    New controllers

---
+++ BinaryStudio.PhotoGallery.Web/Controllers/AuthorizationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinaryStudio.PhotoGallery.Web.ViewModels;
+
+namespace BinaryStudio.PhotoGallery.Web.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        //
+        // GET: /Authorization/
+
+        [HttpGet]
+        public ViewResult AuthorizationForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AuthorizationForm(AuthorizationViewModel authInfo)
+        {
+            /*
+             * проверяем логин и пароль и пускаем дальше если всё ОК
+             * */
+
+            return View("AuthorizationForm", authInfo);
+
+        }
+    }
+}
---
+++ BinaryStudio.PhotoGallery.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BinaryStudio.PhotoGallery.Web.ViewModels;
+
+namespace BinaryStudio.PhotoGallery.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.PartOfDay = hour < 12 ? "Good morning" : "Good afternoon";
+            return View();
+        }
+    }
+}
