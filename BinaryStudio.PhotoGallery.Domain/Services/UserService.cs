9ed47222a2f40ea47345979958b4aa981110e4d7
Antony <antnglvn@gmail.com>
2013-07-20 00:14:48 +0300

    Changes in services hierarchy.

---
+++ BinaryStudio.PhotoGallery.Domain/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using BinaryStudio.PhotoGallery.Database;
+using BinaryStudio.PhotoGallery.Database.ModelInterfaces;
+using BinaryStudio.PhotoGallery.Models;
+
+namespace BinaryStudio.PhotoGallery.Domain.Services
+{
+    internal class UserService : Service, IUserService
+    {
+        public UserService(IUnitOfWorkFactory workFactory) : base(workFactory)
+        {
+        }
+
+        public void RegisterUser(UserModel user)
+        {
+            using (IUnitOfWork unitOfWork = workFactory.GetUnitOfWork())
+            {
+                IUserRepository userRepository = unitOfWork.Users;
+
+                userRepository.Create(user);
+                //todo: it needs some exceptions checking
+            }
+        }
+
+        public void UpdateUser(UserModel user)
+        {
+            using (IUnitOfWork unitOfWork = workFactory.GetUnitOfWork())
+            {
+                IUserRepository userRepository = unitOfWork.Users;
+
+                userRepository.Update(user);
+                //todo: it needs some exceptions checking
+            }
+        }
+
+        public bool CheckUser(UserModel user)
+        {
+            using (IUnitOfWork unitOfWork = workFactory.GetUnitOfWork())
+            {
+                IUserRepository userRepository = unitOfWork.Users;
+
+                return userRepository.Contains(model => model.Equals(user));
+            }
+        }
+    }
+}
