283f7a184df2931b65cfc96ae2b440a2477b0f4e
Astron <astr0n@ukr.net>
2013-07-21 00:51:20 +0300

    added animation for sign in/up pages

---
+++ BinaryStudio.PhotoGallery.Web/Scripts/auth-page.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    
+    // Search DOM elements
+    var changePanel = $("#change-panel");
+    var shadow = $("#full-screen-shadow");
+    var loginPanel = $("#login-panel");
+
+    // Init start values
+    shadow.hide();
+    loginPanel.css({ top: "-20%", opacity: 0.0 });
+    changePanel.css({ top: -60, opacity: 0.0 });
+
+    // Animate all elements
+    loginPanel.animate({ top: "25%", opacity: 1.0 }, 800, function() {
+        changePanel.animate({ top: 0, opacity: 1.0 }, 600);
+    });
+
+    shadow.fadeIn(500);
+});
