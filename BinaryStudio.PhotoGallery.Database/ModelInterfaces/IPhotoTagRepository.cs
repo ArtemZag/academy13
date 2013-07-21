f498a8456e149819b07175b176d43916433b7d6f
Maaak <TowstonogAlexander@gmail.com>
2013-07-22 00:50:34 +0300

    Added interface for PhotoTagRepository

--- BinaryStudio.PhotoGallery.Database/ModelInterfaces/IPhotoTagRepository.cs
+++ BinaryStudio.PhotoGallery.Database/ModelInterfaces/IPhotoTagRepository.cs
@@ -9,5 +9,34 @@ namespace BinaryStudio.PhotoGallery.Database.ModelInterfaces
 {
     public interface IPhotoTagRepository : IBaseRepository<PhotoTagModel>
     {
+        /// <summary>
+        /// Add tag to photo
+        /// </summary>
+        /// <param name="tagName">tag name</param>
+        /// <param name="photoID">photo ID</param>
+        /// <returns></returns>
+        bool AddTag(string tagName, int photoID);
+
+        /// <summary>
+        /// Remove tag from photo
+        /// </summary>
+        /// <param name="tagName">tag name</param>
+        /// <param name="photoID">photo ID</param>
+        /// <returns></returns>
+        bool RemoveTag(string tagName, int photoID);
+
+        /// <summary>
+        /// Gets all photo with tag
+        /// </summary>
+        /// <param name="tagName">tag name</param>
+        /// <returns></returns>
+        IQueryable<PhotoModel> FindPhotosByTag(string tagName);
+
+        /// <summary>
+        /// Gets all tags for photo
+        /// </summary>
+        /// <param name="photoID">photo ID</param>
+        /// <returns></returns>
+        IQueryable<string> GetTagsFromPhoto(int photoID);
     }
 }
