8ffccd49052710f43c9aab84cc059a2bc4e159e2
AwfulVi <Awful.Vi@yandex.ru>
2013-07-21 15:00:57 +0300

    PhotoProcessor Core and test added.

---
+++ BinaryStudio.PhotoGallery.Core/PhotoProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BinaryStudio.PhotoGallery.Core
+{
+    public static class PhotoProcessor
+    {
+        public static Image ImageResize(Image image, int nHeight, int nWidth)
+        {
+            Image result = new Bitmap(nWidth, nHeight);
+            using (var g = Graphics.FromImage(result))
+            {
+                g.CompositingQuality = CompositingQuality.HighQuality;
+                g.SmoothingMode = SmoothingMode.HighQuality;
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(image, 0, 0, nWidth, nHeight);
+                g.Dispose();
+            }
+            return result;
+        }
+
+        public static Image ImageResize(Image image, int nHeight )
+        {
+            var coef = (double)nHeight/(double)image.Height;
+            var nWidth = (int)(image.Width*coef);
+            Image result = ImageResize(image, nHeight, nWidth);
+            return result;  
+        }
+
+    }
+}
---
+++ Tests/BinaryStudio.PhotoGallery.Core.Tests/PhotoProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BinaryStudio.PhotoGallery.Core;
+using NUnit.Framework;
+using System.Drawing;
+using FluentAssertions;
+
+namespace BinaryStudio.PhotoGalery.Core.Tests
+{
+    [TestFixture]
+    public class PhotoProcessorTests
+    {
+        [Test]
+        public void ResizedImageShouldBeCorrect()
+        {
+            Image testImage = new Bitmap(1000,500);
+            Image resizedImage = PhotoProcessor.ImageResize(testImage, 250);
+            resizedImage.Should().NotBeNull();
+            resizedImage.Height.Should().Be(250);
+            resizedImage.Width.Should().Be(500);
+        }
+    }
+}
