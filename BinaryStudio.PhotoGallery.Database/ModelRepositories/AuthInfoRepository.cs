a1b1542bc352400bf5f3ce7851de4948d3ee2a25
Maaak <TowstonogAlexander@gmail.com>
2013-07-22 07:47:27 +0300

    FIX: Added correct method for creating/adding new authentication method

--- BinaryStudio.PhotoGallery.Database/ModelRepositories/AuthInfoRepository.cs
+++ BinaryStudio.PhotoGallery.Database/ModelRepositories/AuthInfoRepository.cs
@@ -3,10 +3,34 @@ using BinaryStudio.PhotoGallery.Models;
 
 namespace BinaryStudio.PhotoGallery.Database.ModelRepositories
 {
-    class AuthInfoRepository : BaseRepository<AuthInfoModel>, IAuthInfoRepository
+    internal class AuthInfoRepository : BaseRepository<AuthInfoModel>, IAuthInfoRepository
     {
-        public AuthInfoRepository(DatabaseContext dataBaseContext) : base(dataBaseContext)
+        public AuthInfoRepository(DatabaseContext dataBaseContext)
+            : base(dataBaseContext)
         {
+        }
+
+        public void AddAuthInfo(int userID, string password, string authProvider)
+        {
+            var auth = new AuthInfoModel
+                {
+                    UserModelID = userID,
+                    UserPassword = password,
+                    AuthProvider = authProvider
+                };
+            base.Create(auth);
+        }
+
+        public void AddAuthInfo(int userID, string password, string authProvider, string authToken)
+        {
+            var auth = new AuthInfoModel
+            {
+                UserModelID = userID,
+                UserPassword = password,
+                AuthProvider = authProvider,
+                AuthProviderToken = authToken
+            };
+            base.Create(auth);
         }
     }
 }
