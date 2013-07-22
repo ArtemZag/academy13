be399c1d00f9952960c70311e45a208bc9dfa5b4
aspivakov90 <spivakov.andrey@gmail.com>
2013-07-22 22:12:55 +0300

    Added AdminPanelController.cs and Views/AdminPanel/Index.cshtml

---
+++ BinaryStudio.PhotoGallery.Web/Controllers/AdminPanelController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+using BinaryStudio.PhotoGallery.Database;
+
+namespace BinaryStudio.PhotoGallery.Web.Controllers
+{
+	[RoutePrefix("AdminPanel")]
+    public class AdminPanelController : Controller
+    {
+		/// <summary>
+		/// Administration page
+		/// </summary>
+		[GET("")]
+        public ActionResult Index()
+        {
+			UnitOfWork unitOfWork = new UnitOfWork();
+			var users = unitOfWork.Users.All().ToList();
+            return View(users);
+        }
+
+    }
+}
