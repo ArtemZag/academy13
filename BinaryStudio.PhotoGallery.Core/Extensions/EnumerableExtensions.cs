156e92f343f1064de1914e916cb63e47c9983160
Antony <antnglvn@gmail.com>
2013-07-22 23:35:32 +0300

    Simple GetPhotos realization.

--- BinaryStudio.PhotoGallery.Core/Extensions/EnumerableExtensions.cs
+++ BinaryStudio.PhotoGallery.Core/Extensions/EnumerableExtensions.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 
 namespace BinaryStudio.PhotoGallery.Core.Extensions
 {
-    public static class EnumerableExtensions 
+    public static class EnumerableExtensions
     {
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            foreach(var item in source)
+            foreach (T item in source)
             {
                 action(item);
+            }
+        }
+
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var seenKeys = new HashSet<TKey>();
+
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
             }
         }
     }
