c5fbd528203044ca159d716ccd6aceea4b0d74a2
Maaak <TowstonogAlexander@gmail.com>
2013-07-22 06:55:39 +0300

    FIX: added correct method for creating/adding new album for user

--- BinaryStudio.PhotoGallery.Database/ModelInterfaces/IAlbumRepository.cs
+++ BinaryStudio.PhotoGallery.Database/ModelInterfaces/IAlbumRepository.cs
@@ -4,5 +4,11 @@ namespace BinaryStudio.PhotoGallery.Database.ModelInterfaces
 {
     public interface IAlbumRepository : IBaseRepository<AlbumModel>
     {
+        /// <summary>
+        /// Adds new album
+        /// </summary>
+        /// <param name="ownerID">User ID that is album's owner</param>
+        /// <returns></returns>
+        void AddAlbum(int ownerID);
     }
 }
