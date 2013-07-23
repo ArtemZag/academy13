59e471d2af09f4cb9659512af5f32991c98f52ba
aspivakov90 <spivakov.andrey@gmail.com>
2013-07-23 15:26:23 +0300

    Added slider with action buttons for user administrations.

--- BinaryStudio.PhotoGallery.Web/Scripts/UserSlider.js
+++ BinaryStudio.PhotoGallery.Web/Scripts/UserSlider.js
@@ -1,8 +1,26 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
 	$('.user-container a').on('click', function (e) {
 		e.preventDefault();
-		var actionDiv = $('.hidden-field').detach();
-		$('.user-main-info', this).after(actionDiv);
-		actionDiv.css('display', 'block');
+		var self = e.target;
+		if (self.tagName.toUpperCase() != 'DIV') {
+			return;
+		}
+		$('.hidden-field').css('display', 'none');
+		if ($('.user-row', this).width() == 230) {
+			$('.user-row', this).animate({
+				width: "100px"
+			}, 1000);
+		} else {
+			$('.user-row').animate({
+				width: "100px"
+			}, { duration: 1000, queue: false });
+			$('.user-row', this).animate({
+				width: "230px"
+			}, { duration: 1000, queue: true });
+			var actionDiv = $('.hidden-field').detach();
+			$('.user-main-info', this).after(actionDiv);
+			setTimeout(function () { actionDiv.css('display', 'block'); }, 1000);
+
+		}
 	});
 });
