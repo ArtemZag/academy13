f071880590d1fdf4c71bd2adde8fcf1a6aad994a
AwfulVi <Awful.Vi@yandex.ru>
2013-07-23 13:47:58 +0300

    Path helper added

---
+++ BinaryStudio.PhotoGallery.Core/Helpers/PathHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BinaryStudio.PhotoGallery.Core.Helpers
+{
+    public static class PathHelper
+    {
+        public static string ContentRoot
+        {
+            get
+            {
+                const string contentVirtualRoot = "~/Content";
+                return VirtualPathUtility.ToAbsolute(contentVirtualRoot);
+            }
+        }
+
+        public static string ImageRoot
+        {
+            get { return string.Format("{0}/{1}", ContentRoot,"Images"); }
+        }
+
+        public static string CssRoot
+        {
+            get { return string.Format("{0}/{1}", ContentRoot, "Css"); }
+        }
+  
+        public static string ImageUrl(string imageFile)
+        {
+            string result = string.Format("{0}/{1}", ImageRoot, imageFile);
+            return result;
+        }
+
+        public static string CssUrl(string cssFile)
+        {
+            string result = string.Format("{0}/{1}", CssRoot, cssFile);
+            return result;
+        }
+    }
+}
