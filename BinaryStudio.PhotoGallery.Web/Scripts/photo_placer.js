3f87736e8ba1776697b64cadfe13e58ad074f785
AwfulVi <Awful.Vi@yandex.ru>
2013-07-23 13:52:57 +0300

    photo_placer script fix

--- BinaryStudio.PhotoGallery.Web/Scripts/photo_placer.js
+++ BinaryStudio.PhotoGallery.Web/Scripts/photo_placer.js
@@ -1,18 +1,41 @@
-﻿$(document).ready(function () {
-    $(window).resize(CalcPhotoSizes);    CalcPhotoSizes(); // todo why is it bad works on webkit? Need fix.
-});function CalcPhotoSizes() {
-    var width = 0;    var firstElemInRow = 0;    var margins = 0;    var wrapperWidth = $('div#photoWrapper').width();    console.debug(wrapperWidth);    var marginPhotoCont = parseInt($('.photoContainer').css('margin-left'))						+ parseInt($('.photoContainer').css('margin-right'));    var photos = $('div#photoWrapper > div > img');
-    jQuery.each(photos, function(i) {
-        width += this.width;
-        margins += marginPhotoCont;
-        if (width > wrapperWidth - margins) {
-            var coef = (wrapperWidth - margins) / width;
-            for (var j = firstElemInRow; j <= i; j++) {
+﻿//$(document).ready(function () {
+//    //photos = $('div#photoWrapper > div > img');
+//    CalcPhotoSizes(); // todo why is it bad works on webkit? Need fix.
+//    $(window).resize(CalcPhotoSizes());
+//    var photos = $('div#photoWrapper > div > img');
+//    jQuery.each(photos, function() {
+//        $(this).css('width', this.width);
+//    });
+//});
+
+////var photos;//function CalcPhotoSizes() {
+//    var width = 0;//    var firstElemInRow = 0;//    var margins = 0;//    var wrapperWidth = $('div#photoWrapper').width();//    console.debug(wrapperWidth);//    var marginPhotoCont = parseInt($('.photoContainer').css('margin-left'))//						+ parseInt($('.photoContainer').css('margin-right'));
+//    var photos = $('div#photoWrapper > div > img');
+//    jQuery.each(photos, function(i) {
+//        width += parseInt($(this).css('width'));
+//        margins += marginPhotoCont;
+//        if (width > wrapperWidth - margins) {
+//            var coef = (wrapperWidth - margins) / width;
+//            for (var j = firstElemInRow; j <= i; j++) {
+//                $(photos[j]).closest(".photoContainer").css('width', (photos[j].width * coef) - 0.2);
+//            }
+//            firstElemInRow = i + 1;
+//            width = 0;
+//            margins = 0;
+//        }
+//    });
+//}
+
+$(document).ready(function () {
+    $(window).resize(CalcPhotoSizes);    CalcPhotoSizes();
+});//$(document).ready(CalcPhotoSizes);function CalcPhotoSizes() {
+    var width = 0;    var firstElemInRow = 0;    var margins = 0;    var wrapperWidth = $('div#photoWrapper').width();    console.debug(wrapperWidth);    var marginPhotoCont = parseInt($('.photoContainer').css('margin-left'))						+ parseInt($('.photoContainer').css('margin-right'));    var photos = $('div#photoWrapper > div > img');    jQuery.each(photos, function (i) {
+        if (i == 1)
+            console.debug(this.width);
+        width += this.width;        margins += marginPhotoCont;        if (width > wrapperWidth - margins) {
+            var coef = (wrapperWidth - margins) / width;            for (var j = firstElemInRow; j <= i; j++) {
                 $(photos[j]).closest(".photoContainer").css('width', (photos[j].width * coef) - 0.2);
-            }
-            firstElemInRow = i + 1;
-            width = 0;
-            margins = 0;
+            }            firstElemInRow = i + 1;            width = 0;            margins = 0;
         }
     });
 }
