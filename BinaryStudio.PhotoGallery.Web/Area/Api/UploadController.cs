b26f84a45046f5ec50734a03202c71e05b39daba
Astron <astr0n@ukr.net>
2013-07-27 00:15:39 +0300

    uploading page

---
+++ BinaryStudio.PhotoGallery.Web/Area/Api/UploadController.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+
+namespace BinaryStudio.PhotoGallery.Web.Area.Api
+{
+	[RoutePrefix("Api/Upload")]
+    public class UploadController : ApiController
+    {
+        [POST]
+        public HttpResponseMessage Index(Container containers, HttpPostedFileBase file)
+        {
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ BinaryStudio.PhotoGallery.Web/Controllers/UploadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+
+namespace BinaryStudio.PhotoGallery.Web.Controllers
+{
+	[RoutePrefix("Upload")]
+    public class UploadController : Controller
+    {
+		[GET("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
