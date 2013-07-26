754d2614624a06ce476e4f4cb269d262a00a540b
Astron <astr0n@ukr.net>
2013-07-26 21:26:09 +0300

    styled validation

--- BinaryStudio.PhotoGallery.Web/Scripts/auth-validate.js
+++ BinaryStudio.PhotoGallery.Web/Scripts/auth-validate.js
@@ -1,5 +1,14 @@
 ﻿$(function () {
-    $('#signin-button').parent().validate({
+
+    var emailTip = new Opentip("#Email", '', { showOn: null, extends: "alert", target: true });
+    var passwordTip = new Opentip($("#Password"), '', { showOn: null, extends: "alert", target: true });
+    var confirmPasswordTip = null;
+    
+    if ($("#ConfirmPassword").length != 0) {
+        confirmPasswordTip = new Opentip("#ConfirmPassword", '', { showOn: null, extends: "alert", target: true });
+    }
+    
+    $('form').validate({
         rules: {
             Email: {
                 required: true,
@@ -11,17 +20,51 @@
             },
             ConfirmPassword: {
                 required: true,
-                minlength: 6,
                 equalTo: "#Password"
             }
         },
         messages: {
             Email: {
-                required: "You must enter Email address",
+                required: "Please enter your e-mail address.",
             },
             Password: {
-                required: "You must enter Password",
-                minlength: "Password should be between 6 and 20 characters"
+                required: "Please enter your password.",
+                minlength: "Password should be 6-20 characters."
+            },
+            ConfirmPassword: {
+                required: "Please repeat your password.",
+                equalTo: "Passwords are not equal."
+            }
+        },
+        errorPlacement: function (error, element) {
+            var tip = null;
+
+            switch (element.attr("name")) {
+                case "Email":
+                    tip = emailTip;
+                    break;
+                case "Password":
+                    tip = passwordTip;
+                    break;
+                case "ConfirmPassword":
+                    tip = confirmPasswordTip;
+                    break;
+            }
+            
+            tip.setContent(error.text());
+            tip.show();
+        },
+        success: function (label, element) {
+            switch ($(element).attr("name")) {
+                case "Email":
+                    emailTip.hide();
+                    break;
+                case "Password":
+                    passwordTip.hide();
+                    break;
+                case "ConfirmPassword":
+                    confirmPasswordTip.hide();
+                    break;
             }
         }
     });
