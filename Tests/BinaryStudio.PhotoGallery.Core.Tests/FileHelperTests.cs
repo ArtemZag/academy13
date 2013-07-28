a5ca8e8d3a84262dfcda78655172ae25345bcf9e
Astron <astr0n@ukr.net>
2013-07-29 02:57:01 +0300

    Tests for FileHelper

--- Tests/BinaryStudio.PhotoGallery.Core.Tests/FileHelperTests.cs
+++ Tests/BinaryStudio.PhotoGallery.Core.Tests/FileHelperTests.cs
@@ -11,13 +11,32 @@ namespace BinaryStudio.PhotoGalery.Core.Tests
         public void FileShouldBeDetectedAsImageFile()
         {
             // setup
-            const string fileName = "somefile.txt";
+            var imageWithTxtExtension = string.Format(@"{0}\{1}", "Content", "imageAsText.txt");
+            var normalImageFile = string.Format(@"{0}\{1}", "Content", "img.jpg");
 
             // body
-            var isImageFile = FileHelper.IsImageFile(fileName);
+            var isTextFormatReallyImage = FileHelper.IsImageFile(imageWithTxtExtension);
+            var isImageFormatReallyImage = FileHelper.IsImageFile(normalImageFile);
 
             // tear down
-            isImageFile.Should().BeTrue();
+            isTextFormatReallyImage.Should().BeTrue();
+            isImageFormatReallyImage.Should().BeTrue();
+        }
+
+        [Test]
+        public void FileShouldBeFailToDetectTextFileAsImageFile()
+        {
+            // setup
+            var emptyFile = string.Format(@"{0}\{1}", "Content", "empty");
+            var textFile = string.Format(@"{0}\{1}", "Content", "text.txt");
+
+            // body
+            var emptyFileIsImage = FileHelper.IsImageFile(emptyFile);
+            var textFileIsImage = FileHelper.IsImageFile(textFile);
+
+            // tear down
+            emptyFileIsImage.Should().BeFalse();
+            textFileIsImage.Should().BeFalse();
         }
     }
 }
