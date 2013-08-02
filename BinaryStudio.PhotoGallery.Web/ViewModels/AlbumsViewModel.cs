57c030ce08746a86281d25f2a03e99de9c4bf01c
San4ezSoft <aleksandr.nosov.1991@mail.ru>
2013-08-02 10:48:58 +0300

    Added albums page View,albums controller, albums page javascript and css stylesheet files, view model for page.

---
+++ BinaryStudio.PhotoGallery.Web/ViewModels/AlbumsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BinaryStudio.PhotoGallery.Web.ViewModels
+{
+    public class AlbumsViewModel
+    {
+        private string userEmail;
+
+    }
+}
