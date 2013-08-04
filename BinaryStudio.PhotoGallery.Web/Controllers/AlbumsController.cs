2efe2009a37dd5395fde2b04a3288e6bb74bcb24
San4ezSoft <aleksandr.nosov.1991@mail.ru>
2013-08-04 15:12:19 +0300

    AlbumsPage

---
+++ BinaryStudio.PhotoGallery.Web/Controllers/AlbumsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+
+namespace BinaryStudio.PhotoGallery.Web.Controllers
+{
+	[RoutePrefix("Albums")]
+    public class AlbumsController : Controller
+    {
+		[GET("")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AjaxResponse(int start, int end)
+        {
+            return Json(new object());
+        }
+    }
+}
