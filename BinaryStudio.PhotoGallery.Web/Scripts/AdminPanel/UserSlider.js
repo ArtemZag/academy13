3af9f7a3e0b1f49eddc65672bb2d8f7b2caa3302
aspivakov90 <spivakov.andrey@gmail.com>
2013-08-07 14:28:01 +0300

    Added viewmodels for AdminPanel page.

---
+++ BinaryStudio.PhotoGallery.Web/Scripts/AdminPanel/UserSlider.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+	$('.user-container a').on('click', function (e) {
+		e.preventDefault();
+		var self = e.target;
+		if (self.tagName.toUpperCase() != 'DIV') {
+			return;
+		}
+		var smaller = 100;
+		var larger = 230;
+		
+		$('.hidden-field').css('display', 'none');
+		if ($('.user-row', this).hasClass('active')) {
+			$('.user-row', this).animate({
+				width: smaller
+			}, 1000).removeClass('active');
+		} else {
+			$('.active').animate({
+				width: smaller
+			}, { duration: 1000, queue: false }).removeClass('active');
+			$('.user-row', this).animate({
+				width: larger
+			}, { duration: 1000, queue: true }).addClass('active');
+			
+			var actionDiv = $('.hidden-field').detach();
+			$('.user-main-info', this).after(actionDiv);
+			setTimeout(function () { actionDiv.css('display', 'block'); }, 1000);
+		}
+	});
+});
