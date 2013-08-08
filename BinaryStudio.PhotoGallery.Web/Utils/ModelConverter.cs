8f40fa89fd1a5e0c7851b897bbdd2957600c8bd2
Maaak <TowstonogAlexander@gmail.com>
2013-08-08 15:41:53 +0300

    FIX: now uses correct userID  -  albumModel.UserModelID (album's owner)

--- BinaryStudio.PhotoGallery.Web/Utils/ModelConverter.cs
+++ BinaryStudio.PhotoGallery.Web/Utils/ModelConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using BinaryStudio.PhotoGallery.Core.PathUtils;
+using BinaryStudio.PhotoGallery.Domain.Services;
 using BinaryStudio.PhotoGallery.Models;
 using BinaryStudio.PhotoGallery.Web.ViewModels;
 using BinaryStudio.PhotoGallery.Web.ViewModels.PhotoPage;
@@ -9,11 +10,13 @@ namespace BinaryStudio.PhotoGallery.Web.Utils
 {
     internal class ModelConverter : IModelConverter
     {
-        private readonly IPathUtil pathUtil;
+        private readonly IPathUtil _pathUtil;
+        private readonly IAlbumService _albumService;
 
-        public ModelConverter(IPathUtil pathUtil)
+        public ModelConverter(IPathUtil pathUtil, IAlbumService albumService)
         {
-            this.pathUtil = pathUtil;
+            _pathUtil = pathUtil;
+            _albumService = albumService;
         }
 
         public UserModel GetModel(RegistrationViewModel registrationViewModel)
@@ -40,15 +43,26 @@ namespace BinaryStudio.PhotoGallery.Web.Utils
 
         public PhotoViewModel GetViewModel(PhotoModel photoModel)
         {
-            var viewModel = new PhotoViewModel
+            // We need to grab photos from album's owner, not from photo's creator.
+            // So needs to take an userID via albumModel.UserModelID
+            var albumModel = _albumService.GetAlbumByID(photoModel.AlbumModelId);
+                
+                var viewModel = new PhotoViewModel
                 {
                     // todo: UserModelId in photoModel != userId which album contain this photo
                     // is PhotoSource necessary in PhotoViewModel?!
-                    PhotoSource =
-                        pathUtil.BuildOriginalPhotoPath(photoModel.UserModelId, photoModel.AlbumModelId, photoModel.Id,
-                                                         photoModel.Format),
 
-                    PhotoThumbSource = pathUtil.BuildThumbnailsPath(photoModel.UserModelId, photoModel.AlbumModelId)
+                    // Maaak: UserModelID is an userID, who has created(added) this photo. 
+                    //        So it can be as equel to userId, which album contain this photo, so not.
+                    //        Look at PhotoModel to check the meaning of property UserModelID
+                    PhotoSource = 
+                        _pathUtil.BuildOriginalPhotoPath(albumModel.UserModelId, photoModel.AlbumModelId, 
+                                                         photoModel.Id,photoModel.Format),
+
+                    // Maaak: I think needs refactoring. Or another method,
+                    //        that will create a path by only one parameter - photoID
+                    PhotoThumbSource = 
+                        _pathUtil.BuildThumbnailsPath(albumModel.UserModelId, photoModel.AlbumModelId)
                                        + @"\" + photoModel.PhotoName + photoModel.Format,
 
                     AlbumId = photoModel.AlbumModelId,
