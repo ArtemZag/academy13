dfe5ef49c68694f87dc1cbdffc59482aecd44eaf
aspivakov90 <spivakov.andrey@gmail.com>
2013-08-12 13:43:11 +0300

    Trouble with ko pushing.

---
+++ BinaryStudio.PhotoGallery.Web/Scripts/AdminPanel/AdminPage.js
@@ -0,0 +1,27 @@
+﻿var Bingally = Bingally || {};
+
+Bingally.AdminPage = Bingally.AdminPage || {};
+
+function AdminPageViewModel() {
+	var self = this;
+
+	self.Users = ko.observableArray();
+
+	self.SelectedUser = ko.observable();
+
+	self.SetCurrentUser = function(user) {
+		self.SelectedUser(user);
+	};
+
+}
+
+Bingally.AdminPage.initializeViewModel = function(initData) {
+	var instanceViewModel = new AdminPageViewModel();
+	$.each(initData.UserViewModels, function (index, user) {
+		instanceViewModel.Users.push(user);
+	});
+	ko.applyBindings(instanceViewModel);
+};
+
+
+
