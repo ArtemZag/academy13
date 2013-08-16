7f0c0b48fa12b3dd1ee2f022b6e223bce0cc1f02
Antony <antnglvn@gmail.com>
2013-08-16 16:01:48 +0300

    Small code changes in AdminPanelController, using outlook email (old) for IEmailSender.

--- BinaryStudio.PhotoGallery.Core/NotificationsUtils/INotificationSender.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BinaryStudio.PhotoGallery.Core.NotificationsUtils
-{
-    public interface INotificationSender
-    {
-        void Send(string fromHost, string fromEmail, string fromPassword, string toEmail, string text);
-    }
-}
