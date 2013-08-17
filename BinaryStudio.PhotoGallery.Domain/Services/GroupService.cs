1e4568864c78ab452a4341ad9f34a98ee200bc81
Maaak <TowstonogAlexander@gmail.com>
2013-08-17 06:14:30 +0300

    Added GroupService

---
+++ BinaryStudio.PhotoGallery.Domain/Services/GroupService.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryStudio.PhotoGallery.Database;
+using BinaryStudio.PhotoGallery.Domain.Exceptions;
+using BinaryStudio.PhotoGallery.Models;
+
+namespace BinaryStudio.PhotoGallery.Domain.Services
+{
+    internal class GroupService : DbService, IGroupService
+    {
+        public GroupService(IUnitOfWorkFactory workFactory) : base(workFactory)
+        {
+        }
+
+        //todo: add check permission for creating group event 
+        public void CreateGroup(int userID, GroupModel groupModel)
+        {
+            using (var unitOfWork = WorkFactory.GetUnitOfWork())
+            {
+                if (unitOfWork.Groups.Find(group => group.GroupName == groupModel.GroupName) != null)
+                {
+                    throw new GroupAlreadyExistException(groupModel.GroupName);
+                }
+
+                unitOfWork.Groups.Add(groupModel);
+                unitOfWork.SaveChanges();
+            }
+        }
+
+        //todo: add check permission for creating group event 
+        public void CreateGroup(int userID, string groupName)
+        {
+            var groupModel = new GroupModel()
+                {
+                    GroupName = groupName,
+                    OwnerID = userID
+                };
+
+            try {this.CreateGroup(userID, groupModel);}
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message, exception);
+            }
+        }
+
+        public GroupModel GetGroup(int groupID)
+        {
+            using (var unitOfWork = WorkFactory.GetUnitOfWork())
+            {
+                try
+                {
+                    return GetGroup(groupID, unitOfWork);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(exception.Message, exception);
+                }
+            }
+        }
+
+        //todo: add check permission for adding group event 
+        public void AddUserToGroup(int ownerID, int userID, int groupID)
+        {
+            using (var unitOfWork = WorkFactory.GetUnitOfWork())
+            {
+                try
+                {
+                    var group = GetGroup(groupID);
+                    var owner = GetUser(ownerID, unitOfWork);
+                    var user = GetUser(userID, unitOfWork);
+
+                    if (group.OwnerID == ownerID || owner.IsAdmin)
+                    {
+                        unitOfWork.Groups.Find(groupID).Users.Add(user);
+                        unitOfWork.SaveChanges();
+                    }
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(exception.Message, exception);
+                }
+            }
+        }
+
+        //todo: add check permission for adding group event 
+        public void AddUserToGroup(int ownerID, int userID, string groupName)
+        {
+            using (var unitOfWork = WorkFactory.GetUnitOfWork())
+            {
+                try
+                {
+                    var group = GetGroup(groupName, unitOfWork);
+
+                    this.AddUserToGroup(ownerID, userID, group.Id);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception(exception.Message, exception);
+                }
+            }
+        }
+    }
+}
---
+++ BinaryStudio.PhotoGallery.Domain/Services/IGroupService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryStudio.PhotoGallery.Models;
+
+namespace BinaryStudio.PhotoGallery.Domain.Services
+{
+    interface IGroupService
+    {
+        /// <summary>
+        /// Creates new group by model
+        /// </summary>
+        /// <param name="userID">Group owner</param>
+        /// <param name="groupModel"></param>
+        void CreateGroup(int userID, GroupModel groupModel);
+
+        /// <summary>
+        /// Creates new group by name
+        /// </summary>
+        /// <param name="userID">Group owner</param>
+        /// <param name="groupName"></param>
+        void CreateGroup(int userID, string groupName);
+
+        /// <summary>
+        /// Gets groupModel by it's ID
+        /// </summary>
+        /// <param name="groupID"></param>
+        /// <returns></returns>
+        GroupModel GetGroup(int groupID);
+
+        /// <summary>
+        /// Adds user to group by groupID
+        /// </summary>
+        /// <param name="ownerID">Group ownerID</param>
+        /// <param name="userID">User to add</param>
+        /// <param name="groupID"></param>
+        void AddUserToGroup(int ownerID, int userID, int groupID);
+
+        /// <summary>
+        /// Adds user to group by groupName
+        /// </summary>
+        /// <param name="ownerID">Group ownerID</param>
+        /// <param name="userID">User to add</param>
+        /// <param name="groupName"></param>
+        void AddUserToGroup(int ownerID, int userID, string groupName);
+    }
+}
