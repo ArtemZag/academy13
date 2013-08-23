959c0550e74cf225d80c9ff06e88f174d87e5963
AwfulVi <Awful.Vi@yandex.ru>
2013-08-23 12:02:31 +0300

    Refactor vertical resizer script & PublicPhotoController greated

---
+++ BinaryStudio.PhotoGallery.Web/Area/Api/PublicPhotoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AttributeRouting;
+using AttributeRouting.Web.Http;
+using BinaryStudio.PhotoGallery.Domain.Services;
+
+namespace BinaryStudio.PhotoGallery.Web.Area.Api
+{
+	[RoutePrefix("Api/PublicPhoto")]
+    public class PublicPhotoController : BaseApiController
+    {
+        private readonly IPhotoService _photoService;
+
+        public PublicPhotoController(IPhotoService photoService)
+        {
+            _photoService = photoService;
+        }
+
+
+    }
+}
---
+++ BinaryStudio.PhotoGallery.Web/Scripts/Account/initVertResizer.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+//    verticalResizer_Module()
+    $("#wrapper").css("height", $(window).height() - 15);
+
+    $(window).on('resize', function () {
+        $("#wrapper").css("height", $(window).height() - 15);
+    });
+
+    $('#myGallery').rtg({
+        imageWidth: 315,
+        spacing: 10,
+        categories: false,
+        lightbox: false,
+        center: false,
+    });
+    var didit = false;
+    var done = false;
+    setTimeout(function () {
+        var bWidth = $('.rtg-images').width();
+        var resizeStep = function () {
+            bWidth = $('.rtg-images').width();
+        };
+        var stepTimer;
+        $(window).resize(function () {
+            if (didit === false) {
+                clearTimeout(stepTimer);
+                stepTimer = setTimeout(resizeStep, 200);
+            }
+        });
+    }, 1000);
+
+    var photoPortion = 20;
+    
+    $.get("/api/photo/allusers", { albumId: 0, skip: 0, take: photoPortion })
+          .done(function (photos) {
+              viewModel.addPhotos(photos);
+          })
+          .fail(function () {
+
+          });
+    
+});
+
+
+function LoginPhotoFlowViewModel() {
+    this.RandomPhotos = ko.observableArray();
+
+    this.addPhotos = function (photos) {
+        RandomPhotos.arrayPushAll(photos);
+    }.bind(this);
+}
+
+var viewModel = new LoginPhotoFlowViewModel();
+ko.applyBindings(viewModel);
