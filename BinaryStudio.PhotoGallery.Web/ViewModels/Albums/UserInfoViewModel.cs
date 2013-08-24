969ef8512fd1c6306f44bc10affc24e8f6d84fd6
Astron <astr0n@ukr.net>
2013-08-24 03:09:01 +0200

    Fix albums page

--- BinaryStudio.PhotoGallery.Web/ViewModels/Albums/UserInfoViewModel.cs
+++ BinaryStudio.PhotoGallery.Web/ViewModels/Albums/UserInfoViewModel.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace BinaryStudio.PhotoGallery.Web.ViewModels.Albums
+﻿namespace BinaryStudio.PhotoGallery.Web.ViewModels.Albums
 {
     public class UserInfoViewModel
     {
         public string FullName { get; set; }
         public string Department { get; set; }
-        public DateTime LastPhotoAdded { get; set; }
         public string AvatarPath { get; set; }
         public int AlbumCount { get; set; }
         public int PhotoCount { get; set; }
