d71102391e35e41ded99563000cf64fbfa0ef5a4
AwfulVi <Awful.Vi@yandex.ru>
2013-08-26 13:29:22 +0300

    Gallery page implemented

---
+++ BinaryStudio.PhotoGallery.Web/Controllers/GalleryController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+using BinaryStudio.PhotoGallery.Domain.Services;
+using BinaryStudio.PhotoGallery.Web.ViewModels;
+
+namespace BinaryStudio.PhotoGallery.Web.Controllers
+{
+    [Authorize]
+    [RoutePrefix("gallery")]
+    public class GalleryController : BaseController
+    {
+        [GET("")]
+        public ActionResult Index()
+        {
+            return View(new InfoViewModel {UserId = User.Id});
+        }
+    }
+}
