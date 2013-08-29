586654b06594fac31567051bd6ac1f71f6bc9859
AwfulVi <Awful.Vi@yandex.ru>
2013-08-29 12:45:50 +0300

    forgot pass scripts, css & html added

---
+++ BinaryStudio.PhotoGallery.Web/Scripts/Account/forgotPass.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $("#forgotPass").click(function () {
+        Bingally.animation($("#loginPanel"), "move",
+            {
+                direction: 'top',
+                method: 'hide',
+                animTime: 500
+            });
+        $("#passRecoveryPanel").removeClass("invisible");
+        Bingally.animation($("#passRecoveryPanel"), "move",
+            {
+                direction: 'top',
+                method: 'show',
+                animTime: 600
+            });
+    });
+
+    $("#backToLogin").click(function() {
+        Bingally.animation($("#passRecoveryPanel"), "move",
+            {
+                direction: 'top',
+                method: 'hide',
+                animTime: 500
+            });
+        Bingally.animation($("#loginPanel"), "move",
+            {
+                direction: 'top',
+                method: 'show',
+                animTime: 600
+            });
+    });
+})
