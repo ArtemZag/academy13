67eed86b2f412b3642de5f5767d2edb34e723797
AwfulVi <Awful.Vi@yandex.ru>
2013-08-30 09:29:47 +0300

    ChangePass.cshtml implemented

---
+++ BinaryStudio.PhotoGallery.Web/Scripts/Account/changePass.js
@@ -0,0 +1,28 @@
+﻿var ChangePass_Init = function(controller) {
+    $(function() {
+        $('#full-screen-shadow').fadeIn();
+        var panel = $('#chPassPanel');
+
+        Bingally.animation(panel, "move",
+        {
+            direction: 'top',
+            method: 'show',
+            animTime: 600
+        });
+
+        panel.find('input[type=password]')
+             .on('focus', function () {
+                 clearErrorMessages();
+             })
+             .on('keypress', function () {
+                 clearErrorMessages();
+             });
+        
+
+
+        function clearErrorMessages() {
+            $('.error-field').html('');
+        }
+
+    });
+}
