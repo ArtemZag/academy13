6cf02d9ac78c16a42a04c7f0d7892903ee5a30a4
Maaak <TowstonogAlexander@gmail.com>
2013-08-30 12:47:45 +0300

    Refactoring

--- BinaryStudio.PhotoGallery.Web/Bootstrapper.cs
+++ BinaryStudio.PhotoGallery.Web/Bootstrapper.cs
@@ -43,7 +43,6 @@ namespace BinaryStudio.PhotoGallery.Web
             container.RegisterType<IGlobalEventsAggregator, GlobalEventsAggregator>(new ContainerControlledLifetimeManager());
             container.RegisterType<INotificationsEventManager, NotificationsEventManager>(new ContainerControlledLifetimeManager());
             container.RegisterType<IGlobalEventsHandler, GlobalEventsHandler>(new ContainerControlledLifetimeManager());
-            container.RegisterType<AuthorizationRoot>();
             container.RegisterInstance(container.Resolve<IGlobalEventsHandler>());
 
             container.RegisterType<ISearchModelConverter, SearchModelConverter>();
